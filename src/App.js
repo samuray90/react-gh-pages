7ab69fb1285b2065b9f5790e14d6175ecb4eae7d
samuray90 <ionutsdaniel53@gmail.com>
2021-01-04 13:19:44 +0200

    repeat useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-
-import { FormControl, MenuItem, Select } from '@material-ui/core';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
         <h1> Covid 19 Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option Two</MenuItem>
-          <MenuItem value="worldwide">Option three</MenuItem>
-          <MenuItem value="worldwide">EU-2021</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </div>
-      
 
       {/* InfoBox */}
       {/* InfoBox */}
