de9c3fb6e5b19d72ed2ea8b6ab73d57d6c91808d
samuray90 <ionutsdaniel53@gmail.com>
2021-01-04 14:14:33 +0200

    45% of the code needs to be retiped

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { FormControl, MenuItem, Select } from "@material-ui/core";
 import "./App.css";
+import InfoBox from "./InfoBox";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
@@ -18,12 +20,19 @@ function App() {
     };
     getCountriesData();
   }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    console.log("adsadsadasdadad", countryCode);
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app__header">
         <h1> Covid 19 Tracker</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
@@ -31,14 +40,11 @@ function App() {
         </FormControl>
       </div>
 
-      {/* InfoBox */}
-      {/* InfoBox */}
-      {/* InfoBox */}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases={45} total={20000} />
+        <InfoBox title="Recovered" cases={4522} total={521512} />
+        <InfoBox title="Deaths" cases={85} total={8521} />
+      </div>
     </div>
   );
 }
